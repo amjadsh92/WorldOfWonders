07a825d750045e44e5043bc834fa4ac6a37b2ea6
amjadsh92 <amjadsharafeddien@gmail.com>
2024-11-02 13:12:27 +0200

    fetched the images of 10 world wonders

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-
-
 const fetchWorldWonders = async () => {
     const response = await fetch("https://www.world-wonders-api.org/v0/wonders")
     const data = await response.json();
-    console.log(data);
+    console.log(data[0].links.images[0]);
+    
+    return data
 
 };
 
-fetchWorldWonders()
+
+const worldWonderCards = async () => {
+
+    let worldWonders = await fetchWorldWonders()
+    worldWonders = worldWonders.slice(0,10)
+    //console.log(worldWonders)
+    const body = document.body; 
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "wrapper");
+
+    worldWonders.forEach((worldWonder) => {
+
+    const image = document.createElement("img");
+    image.setAttribute("src", worldWonder.links.images[0])
+    wrapper.appendChild(image);
+    body.appendChild(wrapper)
+
+
+
+
+
+        
+
+
+
+    })
+
+}
+
+
+worldWonderCards()
+
+
+
+
